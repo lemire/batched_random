40cafc5b10ce1f046f60abe9b8da2cd4c9ac1030
NevinBR <nevin.brackettrozinsky@gmail.com>
2024-04-22 22:16:31 -0400

    Added 2-4-6 method to template_shuffle.h

--- include/template_shuffle.h
+++ include/template_shuffle.h
@@ -94,6 +94,43 @@ extern void shuffle_2_4(RandomIt first, RandomIt last, URBG &&g) {
     partial_shuffle_64b(first, i, i - 1, 24, g);
   }
 }
+
+// This is a template function that shuffles the elements in the range [first,
+// last)
+template <class RandomIt, class URBG>
+extern void shuffle_2_4_6(RandomIt first, RandomIt last, URBG &&g) {
+
+  uint64_t i = std::distance(first, last);
+  for (; i > 0x40000000; i--) {
+    partial_shuffle_64b(first, i, 1, i, g);
+  }
+
+  // Batches of 2 for sizes between 2^14 and 2^30 elements
+  uint64_t bound = i * (i - 1);
+  for (; i > 0x4000; i -= 2) {
+    bound = partial_shuffle_64b(first, i, 2, bound, g);
+  }
+
+  // Batches of 4 for sizes up to 2^14 elements
+  // compute i * (i-1) * (i-2) * (i-3) with only 2 multiplications
+  bound = i * (i - 3);
+  bound *= bound + 2;
+  for (; i > 512; i -= 4) {
+    bound = partial_shuffle_64b(first, i, 4, bound, g);
+  }
+
+  // Batches of 6 for sizes up to 2^9 elements
+	// compute i * (i-1) * ... * (i-5) with only 3 multiplications
+	bound = i * (i - 5);
+	bound *= (bound + 4) * (bound + 6);
+	for (; i > 6; i -= 6) {
+		bound = partial_shuffle_64b(first, i, 6, bound, g);
+	}
+
+  if (i > 1) {
+    partial_shuffle_64b(first, i, i - 1, 720, g);
+  }
+}
 } // namespace batched_random
 
 #endif // TEMPLATE_SHUFFLE_H
