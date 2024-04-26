e9826ecf73f1d5d9dec22a0f9e41a2f2028305d7
NevinBR <nevin.brackettrozinsky@gmail.com>
2024-04-25 22:41:25 -0400

    Update shuffle_batch

--- src/random_bounded.c
+++ src/random_bounded.c
@@ -110,9 +110,18 @@ void shuffle_pcg64(uint64_t *storage, uint64_t size) {
 }
 
 void shuffle_naive_batch(uint64_t *storage, uint64_t size) {
-  uint64_t i;
+  uint64_t i = size;
   uint64_t nextpos, nextpos1, nextpos2, tmp, val;
-  for (i = size; i > 2; i -= 2) {
+  
+  for (; i > 0x40000000; i--) {
+    nextpos = random_bounded(i);
+    tmp = storage[i - 1];   // likely in cache
+    val = storage[nextpos]; // could be costly
+    storage[i - 1] = val;
+    storage[nextpos] = tmp; // you might have to read this store later
+  }
+  
+  for (; i > 1; i -= 2) {
     nextpos1 = random_bounded(i);
     nextpos2 = random_bounded(i - 1);
 
@@ -125,42 +134,34 @@ void shuffle_naive_batch(uint64_t *storage, uint64_t size) {
     storage[i - 2] = val;
     storage[nextpos2] = tmp; // you might have to read this store later
   }
-  for (; i > 1; i--) {
+}
+
+
+void shuffle_batch(uint64_t *storage, uint64_t size) {
+  uint64_t i = size;
+  uint64_t result[2];
+  uint64_t nextpos, tmp, val;
+  
+  for (; i > 0x40000000; i--) {
     nextpos = random_bounded(i);
     tmp = storage[i - 1];   // likely in cache
     val = storage[nextpos]; // could be costly
     storage[i - 1] = val;
     storage[nextpos] = tmp; // you might have to read this store later
   }
-}
-
-
-void shuffle_batch(uint64_t *storage, uint64_t size) {
-  uint64_t i;
-  uint64_t result[4];
-  uint64_t nextpos, tmp, val;
-  i = size;
-  if (i <= 0x40000000) {
-    for (i = size; i > 2; i -= 2) {
-      batch_random(i, 2, i * (i - 1), result);
-      nextpos = result[0];
-      tmp = storage[i - 1];   // likely in cache
-      val = storage[nextpos]; // could be costly
-      storage[i - 1] = val;
-      storage[nextpos] = tmp; // you might have to read this store later
-      nextpos = result[1];
-      tmp = storage[i - 2];   // likely in cache
-      val = storage[nextpos]; // could be costly
-      storage[i - 2] = val;
-      storage[nextpos] = tmp; // you might have to read this store later
-    }
-  }
-
-  for (; i > 1; i--) {
-    nextpos = random_bounded(i);
+  
+  uint64_t bound = i * (i  - 1);
+  for (; i > 1; i -= 2) {
+    bound = batch_random(i, 2, bound, result);
+    nextpos = result[0];
     tmp = storage[i - 1];   // likely in cache
     val = storage[nextpos]; // could be costly
     storage[i - 1] = val;
+    storage[nextpos] = tmp; // you might have to read this store later
+    nextpos = result[1];
+    tmp = storage[i - 2];   // likely in cache
+    val = storage[nextpos]; // could be costly
+    storage[i - 2] = val;
     storage[nextpos] = tmp; // you might have to read this store later
   }
 }
